70ca974d296d427dad284876bd29da788311d5bd
ArmandoGR24 <armandogzzdelarosa04@gmail.com>
2024-09-09 13:55:24 -0600

    Splash y Alertas de la app en login y registro

--- Taskly/app/src/main/java/com/codigomaestro/taskly/AuthActivity.java
+++ Taskly/app/src/main/java/com/codigomaestro/taskly/AuthActivity.java
@@ -1,6 +1,12 @@
 package com.codigomaestro.taskly;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +14,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.developer.kalert.KAlertDialog;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class AuthActivity extends AppCompatActivity {
+
+    private EditText user, pass;
+    private Button btn_login, btn_register;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,43 @@ public class AuthActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mAuth = FirebaseAuth.getInstance();
+
+        user = findViewById(R.id.user_box);
+        pass = findViewById(R.id.pass_box);
+        btn_login = findViewById(R.id.btn_login);
+        btn_register = findViewById(R.id.btn_registrar);
+
+        btn_login.setOnClickListener(v -> {
+            String email = user.getText().toString();
+            String password = pass.getText().toString();
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(AuthActivity.this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(AuthActivity.this, "Autenticación exitosa.", Toast.LENGTH_SHORT).show();
+                            // Navegar a la siguiente actividad o actualizar la UI
+                            Intent intent = new Intent(AuthActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(AuthActivity.this, "Autenticación fallida.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        });
+
+        btn_register.setOnClickListener(v -> {
+            Intent intent = new Intent(AuthActivity.this, RegisterActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
     }
 }
--- Taskly/app/src/main/java/com/codigomaestro/taskly/RegisterActivity.java
+++ Taskly/app/src/main/java/com/codigomaestro/taskly/RegisterActivity.java
@@ -1,6 +1,10 @@
 package com.codigomaestro.taskly;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText user, pass;
+    private Button btn_register;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,38 @@ public class RegisterActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mAuth = FirebaseAuth.getInstance();
+
+        user = findViewById(R.id.user_box);
+        pass = findViewById(R.id.pass_box);
+        btn_register = findViewById(R.id.btn_registrar);
+
+        btn_register.setOnClickListener(v -> {
+            String email = user.getText().toString();
+            String password = pass.getText().toString();
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(RegisterActivity.this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(RegisterActivity.this, "Usuario registrado exitosamente.", Toast.LENGTH_SHORT).show();
+                            // Navegar a la siguiente actividad o actualizar la UI
+                            Intent intent = new Intent(RegisterActivity.this, AuthActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Error al registrar usuario.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+        });
+
     }
 }
